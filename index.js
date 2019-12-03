061e74606b580f1702d1406b55f48438d1af75f0
Alison Reed <alison.michelle.reed@gmail.com>
2019-12-03 18:13:33 -0500

    Add scripts to package.json to integrate create-react-app client with node app

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello World!!'))
 
+if (process.env.NODE_ENV === "production") {
+	// serve production assests
+	app.use(express.static("referral-app/build"));
+	// serve html file if non specified route
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "referral-app", "build", "index.html"));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
