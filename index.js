a66984ffe88b0cf7064509487d87f0da53218117
Alison Reed <alison.michelle.reed@gmail.com>
2019-12-03 13:14:15 -0500

    Create basic node server to serve production assets

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+	// serve production assests
+	app.use(express.static("client/build"));
+	// serve html file if non specified route
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "referral-app", "build", "index.html"));
+	});
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
