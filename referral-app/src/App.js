88e8423f95690beaa821ce8017c48cf17ae040d4
Alison Reed <alison.michelle.reed@gmail.com>
2019-12-04 14:31:34 -0500

    Add react router and create root route for link page

--- referral-app/src/App.js
+++ referral-app/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import LinkPage from './linkPage/LinkPage'
 import store from './redux/store'
 
-function App() {
+const App = () => {
   return (
     <Provider store={ store }>
       <div className="App">
-        <LinkPage />
+        <Router >
+          <Switch>
+            <Route exact path='/'>
+              <LinkPage />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </Provider>
   );
