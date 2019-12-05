46555f116ff35a2eb006e077370a9c8187731afb
Alison Reed <alison.michelle.reed@gmail.com>
2019-12-05 18:28:04 -0500

    Add nav bar with back to home link to landing page

--- referral-app/src/landingPages/LandingPage.js
+++ referral-app/src/landingPages/LandingPage.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
+import './LandingPage.scss'
+
+const data = [{ name: 'Home', action: '/' }]
 
 const LandingPage = () => {
   const { pageTitle } = useParams()
   return (
     <div>
-      <h1>{ pageTitle }</h1>
+      <header>
+        <h1>{ pageTitle }</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
