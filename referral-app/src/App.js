6395317a1dc0ec9a4e5e73e562e5d25ad1812c4e
Alison Reed <alison.michelle.reed@gmail.com>
2019-12-05 17:57:41 -0500

    Add footer with creator information

--- referral-app/src/App.js
+++ referral-app/src/App.js
@@ -4,22 +4,26 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.scss';
 import LinkPage from './linkPage/LinkPage'
 import LandingPage from './landingPages/LandingPage'
+import Footer from './Footer'
 import store from './redux/store'
 
 const App = () => {
   return (
     <Provider store={ store }>
       <div className="App">
-        <Router >
-          <Switch>
-            <Route path='/:pageTitle'>
-              <LandingPage />
-            </Route>
-            <Route exact path='/'>
-              <LinkPage />
-            </Route>
-          </Switch>
-        </Router>
+        <main>
+          <Router >
+            <Switch>
+              <Route path='/:pageTitle'>
+                <LandingPage />
+              </Route>
+              <Route exact path='/'>
+                <LinkPage />
+              </Route>
+            </Switch>
+          </Router>
+        </main>
+        <Footer />
       </div>
     </Provider>
   );
---
+++ referral-app/src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer>
+    <p>
+      This app was created by Alison Reed to fulfill requirements of the Ambassador coding challenge
+    <br/>
+      {`Source code can be found at: `}
+      <a href='https://github.com/shimoal/coding-challenge' target="_blank">
+        github.com/shimoal/coding-challenge</a>
+    </p>
+  </footer>
+)
+
+export default Footer
